aab535fd9f699de727f9235958f93e07ef015f42
Reg Henderson <reggiepv@gmail.com>
2019-07-27 13:14:48 -0400

    initial project version

---
+++ VCSBS/Chapter 11/ParamsArray/ParamsArray/Util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Util
+    {
+        public static int Sum(params int[] paramList)
+        {
+            if (paramList == null)
+            {
+                throw new ArgumentException("Util.Sum: null parameter list");
+            }
+            if (paramList.Length == 0)
+            {
+                throw new ArgumentException("Util.Sum: empty parameter list");
+            }
+
+            int sumTotal = 0;
+            foreach (int i in paramList)
+            {
+                sumTotal += i;
+            }
+            return sumTotal;
+        
+        }
+    }
+}
---
+++ VCSBS/Chapter 7/Classes/Classes/Point.cs
@@ -0,0 +1,48 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        private int x, y;
+
+        private static int objectCount = 0;
+
+        public int ObjectCountOld()
+        {
+            return objectCount;
+        }
+        public static int ObjectCount() => objectCount;
+
+        public Point()
+        {
+            objectCount++;
+            x = 0;
+            y = 0;
+            Console.WriteLine("Default constructor is called");
+        }
+
+        public Point(int x, int y)
+        {
+            objectCount++;
+            this.x = x;
+            this.y = y;
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+   
+        public double DistanceToPoint(Point other)
+        {
+            int xDiff = x - other.x;
+            int yDiff = y - other.y;
+            double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+            return distance;
+        }
+
+    }
+}
